6f342c69b7c7569cd82b2cb717e892ae9e274b12
thang44hdai <thangquang8888@gmail.com>
2024-03-06 14:59:54 +0700

    branch 2

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'test'),
+      home: const MyHomePage(title: 'test1'),
     );
   }
 }
