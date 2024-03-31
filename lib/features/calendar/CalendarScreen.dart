e43c3d6c896be4081672b25d2b56ceb589eeecf5
Nguyen Quang Thang <102580944+thang44hdai@users.noreply.github.com>
2024-03-31 14:30:28 +0700

    refactor features calendar

--- lib/features/calendar/CalendarScreen.dart
+++ lib/features/calendar/CalendarScreen.dart
@@ -69,28 +69,13 @@ class CalendarBody extends StatefulWidget {
 }
 
 class _CalendarBodyState extends State<CalendarBody> {
-  DateTime _forcusedDay = DateTime.now();
-  DateTime? _selectedDay;
+  DateTime today = DateTime.now();
   Map<DateTime, List<Event>> events = {};
-
-  @override
-  void initState() {
-    super.initState();
-    _selectedDay = _forcusedDay;
-    _selectedEvents = ValueNotifier(_getEventsForDay(_selectedDay!));
-  }
 
   void _onDaySelected(DateTime day, DateTime focusedDay) {
     setState(() {
-      _forcusedDay = day;
-      _selectedEvents = ValueNotifier(_getEventsForDay(_selectedDay!));
+      today = day;
     });
-  }
-
-  late final ValueNotifier<List<Event>> _selectedEvents;
-
-  List<Event> _getEventsForDay(DateTime day) {
-    return events[day] ?? [];
   }
 
   @override
@@ -106,12 +91,11 @@ class _CalendarBodyState extends State<CalendarBody> {
               titleCentered: true,
             ),
             availableGestures: AvailableGestures.all,
-            focusedDay: _forcusedDay,
+            focusedDay: today,
             firstDay: DateTime.utc(2024),
             lastDay: DateTime.utc(2025),
             onDaySelected: _onDaySelected,
-            eventLoader: _getEventsForDay,
-            selectedDayPredicate: (day) => isSameDay(day, _forcusedDay),
+            selectedDayPredicate: (day) => isSameDay(day, today),
             calendarStyle: CalendarStyle(
               todayDecoration: BoxDecoration(
                 color: Colors.red[200],
@@ -129,28 +113,6 @@ class _CalendarBodyState extends State<CalendarBody> {
           height: 0.5,
           color: Colors.grey,
         ),
-        SizedBox(height: 10),
-        ValueListenableBuilder(
-          valueListenable: _selectedEvents,
-          builder: (context, value, _) {
-            return ListView.builder(itemBuilder: (context, index) {
-              return Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: ListTile(
-                  onTap: () {
-                    events.addAll({
-                      _selectedDay!: [Event(title: event_controller.text)]
-                    });
-                    Navigator.pop(context);
-                    _selectedEvents.value = _getEventsForDay(_selectedDay!);
-                  },
-                ),
-              );
-            });
-          },
-        )
       ],
     );
   }
